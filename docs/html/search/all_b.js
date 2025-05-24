8c0cc2d09a3f901caf32c24c8526793c19f0efc5
alessandropedone <alessandro.pedone02@gmail.com>
2025-05-25 01:28:43 +0200

    update docs

--- docs/html/search/all_b.js
+++ docs/html/search/all_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['parallel_20solver_20for_20the_20laplace_20equation_0',['A matrix–free parallel solver for the Laplace equation',['../index.html#autotoc_md0',1,'']]],
-  ['plot_1',['plot',['../namespaceplot.html',1,'plot'],['../main_8cpp.html#a3137c56db008026a3b63daecf364241f',1,'plot():&#160;main.cpp']]],
+  ['plot_1',['plot',['../namespaceplot.html',1,'plot'],['../namespaceplot.html#a31925a5dbaf98843c43e4fae87af3907',1,'plot::plot()']]],
   ['plot_2ehpp_2',['plot.hpp',['../plot_8hpp.html',1,'']]],
   ['plotter_3',['Plotter',['../classplot_1_1Plotter.html',1,'plot']]],
   ['printdatasummary_4',['printDataSummary',['../classplot_1_1Plotter.html#abe5a6a28b60bf68ede39e90b9371d50f',1,'plot::Plotter']]]
--- docs/html/search/functions_4.js
+++ docs/html/search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['plot_0',['plot',['../main_8cpp.html#a3137c56db008026a3b63daecf364241f',1,'main.cpp']]],
+  ['plot_0',['plot',['../namespaceplot.html#a31925a5dbaf98843c43e4fae87af3907',1,'plot']]],
   ['printdatasummary_1',['printDataSummary',['../classplot_1_1Plotter.html#abe5a6a28b60bf68ede39e90b9371d50f',1,'plot::Plotter']]]
 ];
